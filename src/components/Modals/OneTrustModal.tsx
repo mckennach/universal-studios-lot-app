c0139a7532ce22da6d3b585837c0aa61c182c73b
Chris McKenna <hello@mckennach.com>
2023-10-12 09:52:00 -0400

    fixing onetrust popup

--- src/components/Modals/OneTrustModal.tsx
+++ src/components/Modals/OneTrustModal.tsx
@@ -1,26 +1,28 @@
 import { useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, NativeModules } from 'react-native';
 import OTPublishersNativeSDK from "react-native-onetrust-cmp";
 const { EXPO_PUBLIC_ONETRUST_DOMAIN, EXPO_PUBLIC_ONETRUST_IOS } = process.env;
 const OneTrustModal = () => {
-    // const { OTPublishersNativeSDK } = NativeModules;
+    
     useEffect(() => {
-        OTPublishersNativeSDK.startSDK(
-          EXPO_PUBLIC_ONETRUST_DOMAIN as string,
-          EXPO_PUBLIC_ONETRUST_IOS as string,
-          "en",
-          {
-            countryCode: "US",
-          },
-          true
-        )
-          .then((responseObject) => {
-            console.info("Download status is " + responseObject.status);
-            // get full JSON object from responseObject.responseString
-          })
-          .catch((error) => {
-            console.error(`OneTrust download failed with error ${error}`);
-          });
+      // console.log(OTPublishersNativeSDK);
+      // console.log(NativeModules);
+        // OTPublishersNativeSDK.startSDK(
+        //   EXPO_PUBLIC_ONETRUST_DOMAIN as string,
+        //   EXPO_PUBLIC_ONETRUST_IOS as string,
+        //   "en",
+        //   {
+        //     countryCode: "US",
+        //   },
+        //   true
+        // )
+        //   .then((responseObject) => {
+        //     console.info("Download status is " + responseObject.status);
+        //     // get full JSON object from responseObject.responseString
+        //   })
+        //   .catch((error) => {
+        //     console.error(`OneTrust download failed with error ${error}`);
+        //   });
        
     }, []);
 
